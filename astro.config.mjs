9a00f66705133b19350d0638dcb899d4e8ff9879
Yash Pandya <yash1094@gmail.com>
2026-01-20 10:20:13 -0500

    added ssr netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  output: 'server', adapter: netlify()
 });
